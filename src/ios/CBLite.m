7b607357db2fbb944193358716a16bcf412b34d4
Diego Vallarta <diego.vallarta@gmail.com>
2015-12-22 16:46:11 +0100

    updated plugin to be compatible with cordova 4.x.x

--- src/ios/CBLite.m
+++ src/ios/CBLite.m
@@ -10,14 +10,8 @@
 
 @synthesize liteURL;
 
-- (id) initWithWebView:(UIWebView*)theWebView
-{
-    self = [super initWithWebView:theWebView];
-    if (self) {
-        // todo check domain whitelist to give devs a helpful error message
-        [self launchCouchbaseLite];
-    }
-    return self;
+-(void) pluginInitialize{
+    [self launchCouchbaseLite];
 }
 
 - (void)getURL:(CDVInvokedUrlCommand*)urlCommand
